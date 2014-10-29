f52361def9892da1269881a171fb74cc1fe31755
Joel Friedly <joelfriedly@gmail.com>
2014-10-28 22:03:34 -0700

    Refactor totalling script to handle Eurozone better

--- total.py
+++ total.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import copy
 
 import utils
 
@@ -11,7 +12,14 @@ def parse_args():
     argparser.add_argument("country",
                            type=str,
                            help="Country to sum.")
-    return argparser.parse_args()
+    args = argparser.parse_args()
+
+    # Allow file paths
+    if args.country.startswith("data/"):
+        args.country = args.country[5:]
+    if args.country.endswith(".json"):
+        args.country = args.country[:-5]
+    return args
 
 
 def sum_units(desired_unit, totals, divisions, divisor):
@@ -41,27 +49,44 @@ def sum_units(desired_unit, totals, divisions, divisor):
                                          divisor * breakdown['value'])
 
 
-def total(country):
+def total(country, count_obsolete=False):
     """ Calculates the non-obsolete total value of a country's inventory
 
     :param country:  A country dictionary loaded from a file in the data dir
+    :param count_obsolete:  Boolean whether or not to count obsolete pieces
     :returns:  The non-obsolete total for this country, expressed in terms of
                the largest unit
     """
     totals = {denomination: 0 for denomination in country['denominations']}
     for unit in country['inventory']:
-        if not unit['obsolete']:
+        if not unit['obsolete'] or count_obsolete:
             totals[unit['denomination']] += unit['value']
     # The country's denominations should be ordered from largest to smallest
     biggest_unit = country['denominations'][0]
-    return sum_units(biggest_unit, totals, country['divisions'], 1)
+    divisions = copy.deepcopy(country['divisions'])
+    return sum_units(biggest_unit, totals, divisions, 1)
+
+
+def format_country(country):
+    """ Print one country's total """
+    out = "%(name_long)s:  %(currency_total).2f %(biggest_unit)s"
+    non_obsolete_total = total(country)
+    if non_obsolete_total == 0.0:
+        out = "%(name_long)s:  %(currency_total).2f obsolete %(biggest_unit)s"
+        obsolete_total = total(country, count_obsolete=True)
+    return out % dict(name_long=country['name_long'],
+                      currency_total=(non_obsolete_total or obsolete_total),
+                      biggest_unit=country['denominations'][0])
 
 
 def main():
     args = parse_args()
     country = utils.load_country(args.country)
-    currency_total = total(country)
-    print "%f %s" % (currency_total, country['denominations'][0])
+    out = format_country(country)
+    # Currency from Eurozone countries is all obsolete, but I have Euros
+    if country['name_long'] in utils.EUROZONE_COUNTRIES:
+        out = "%s (%s)" % (out, format_country(utils.load_country('eurozone')))
+    print out
 
 
 if __name__ == "__main__":
